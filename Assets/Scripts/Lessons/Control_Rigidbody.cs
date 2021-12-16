d8c59ff09db1f46dc3c95185de70f2481769b43f
frtike <firatike.27@gmail.com>
2021-12-16 18:57:56 +0300

    +basic physics

---
+++ Assets/Scripts/Lessons/Control_Rigidbody.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Lessons
+{
+    public class Control_Rigidbody : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody _rigidbody;
+        [SerializeField] private ControlRigidbodySettings _settings;
+
+        private void Update()
+        {
+            bool spaceKeyDown = Input.GetKeyDown(KeyCode.Space);
+            if (spaceKeyDown)
+            {
+                _rigidbody.AddForce(_settings.JumpForce, ForceMode.Impulse);
+            }
+        }
+    }
+}
