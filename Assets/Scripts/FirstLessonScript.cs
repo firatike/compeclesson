e7fc0dbaa3ca4597dcf86122b8f6fae5ef52b722
frtike <firatike.27@gmail.com>
2021-12-16 09:52:56 +0300

    +created first lesson script

---
+++ Assets/Scripts/FirstLessonScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace LessonsBasic
+{
+    public class FirstLessonScript : MonoBehaviour
+    {
+
+        void Start()
+        {
+
+        }
+
+
+        void Update()
+        {
+
+        }
+    }
+}
