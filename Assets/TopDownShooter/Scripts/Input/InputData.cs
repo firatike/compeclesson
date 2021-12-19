d23d5614e39abdaf3889c7754938d64ddafa8351
frtike <firatike.27@gmail.com>
2021-12-19 12:37:54 +0300

    +refactor : input system

--- Assets/TopDownShooter/Scripts/Input/InputData.cs
+++ Assets/TopDownShooter/Scripts/Input/InputData.cs
@@ -9,5 +9,63 @@ namespace TopDownShooter.PlayerInput
     {
         public float Horizontal;
         public float Vertical;
+
+        [Header("Axis Base Control")]
+        [SerializeField] private bool _axisActive;
+        [SerializeField] private string axisNameHorizontal;
+        [SerializeField] private string axisNameVertical;
+
+        [Header("Key Base Control")]
+        [SerializeField] private bool _keyBaseHorizontalActive;
+        [SerializeField] private KeyCode PositiveHorizontalKeyCode;
+        [SerializeField] private KeyCode NegativeHorizontalKeyCode;
+        [SerializeField] private bool _keyBaseVerticalActive;
+        [SerializeField] private KeyCode PositiveVerticalKeyCode;
+        [SerializeField] private KeyCode NegativeVerticalKeyCode;
+        [SerializeField] private float _increaseAmount = 0.015f;
+
+        public void ProcessInput()
+        {
+            if (_axisActive)
+            {
+                Horizontal = Input.GetAxis(axisNameHorizontal);
+                Vertical = Input.GetAxis(axisNameVertical);
+            }
+            else
+            {
+                if (_keyBaseHorizontalActive)
+                {
+                    if (_keyBaseHorizontalActive)
+                    {
+                        KeyBaseAxisControl(ref Horizontal, PositiveHorizontalKeyCode, NegativeHorizontalKeyCode);
+                    }
+                    if (_keyBaseVerticalActive)
+                    {
+                        KeyBaseAxisControl(ref Vertical, PositiveVerticalKeyCode, NegativeVerticalKeyCode);
+                    }
+                }
+            }
+        }
+
+        private void KeyBaseAxisControl(ref float value, KeyCode positive, KeyCode negative)
+        {
+            bool positiveActive = Input.GetKey(positive);
+            bool negativeActive = Input.GetKey(negative);
+            if (positiveActive)
+            {
+                value += _increaseAmount;
+            }
+            else if (negativeActive)
+            {
+                value -= _increaseAmount;
+            }
+            else
+            {
+                value = 0;
+            }
+
+            value = Mathf.Clamp(value, -1, 1);
+        }
+
     }
 }
